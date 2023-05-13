4674059a8634b34de70242d377b34fa455cb4e5b
Harshad <harsh.haou@gmail.com>
2023-05-13 10:13:02 +0530

    error message

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import "./App.css";
 function App() {
   async function requestPermission() {
     const permission = await Notification.requestPermission();
-    if (permission === "granted") {
-      // Generate Token
-      const token = await getToken(messaging, {
-        vapidKey:
-          "BH4iHjV4KWPAFPtA_EsNDALS4rnJaGu8HSeiG9lczuDq1d3_d0oL1rtq2Ir9ORHi0vzyn81G293cDufaBOxG0TI",
-      });
-      console.log("Token Gen", token);
-      // Send this token  to server ( db)
-    } else if (permission === "denied") {
-      alert("You denied for the notification");
+    try {
+      if (permission === "granted") {
+        // Generate Token
+        const token = await getToken(messaging, {
+          vapidKey:
+            "BH4iHjV4KWPAFPtA_EsNDALS4rnJaGu8HSeiG9lczuDq1d3_d0oL1rtq2Ir9ORHi0vzyn81G293cDufaBOxG0TI",
+        });
+        console.log("Token Gen", token);
+        // Send this token  to server ( db)
+      } else if (permission === "denied") {
+        alert("You denied for the notification");
+      }
+    } catch (error) {
+      console.log("erroooo", error);
     }
   }
 
